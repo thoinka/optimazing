d3134b635ac52b54d649430c8ae9f55bbe3f82ed
Tobias Hoinka <thoinka@gmail.com>
2023-06-27 10:43:02 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,17 @@ setup(
     description="Wrapper around scipy.optimize.minimize",
     author="Tobias Hoinka",
     author_email="thoinka@gmail.com",
+    url="https://github.com/thoinka/optimazing",
+    keywords=["Optimization", "Scipy", "Minimization"],
     packages=["optimazing"],
     install_requires=["scipy", "numpy", "pandas"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
